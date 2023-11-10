2ce576618bd438f7093f0cfb3204fa46dbfbefe5
Adebozz <ademolaadeigbe3@gmail.com>
2023-11-10 21:44:09 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import styled from 'styled-components';
 
 const Navigation = styled.nav`
